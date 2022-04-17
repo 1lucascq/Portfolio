397874c40403c07ca6e2336b1665bee250c045e3
1lucascq <1lucascq@gmail.com>
2022-04-17 02:53:26 -0300

    setup: install styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/globalStyles';
+import theme from './styles/theme';
 
 function App(): JSX.Element {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
       bla
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+const theme = {
+  colors: {
+    background: '#121214',
+    text: '#e1e1e4',
+    primary: '#e800a1',
+  },
+};
+
+export default theme;

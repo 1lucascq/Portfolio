23218732c846adf9079346b4cdfbcc46a252b27b
1lucascq <1lucascq@gmail.com>
2022-04-17 01:12:18 -0300

    setup: install eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 
-function App() {
+function App(): JSX.Element {
   return (
-    <div>
+    <>
       bla
-    </div>
+    </>
   );
 }
 

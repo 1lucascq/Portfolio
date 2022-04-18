ac852a0e70605dd63952057813a32e74267c6732
1lucascq <1lucascq@gmail.com>
2022-04-18 15:42:08 -0300

    styles: enhance styles in About section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,54 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
 import SectionContainer from '../components/SectionContainer';
-
-const AboutContainer = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: justify;
-
-  div {
-    display: flex;
-    flex-direction: column;
-    gap: 1em;
-    width: 100%;
-  }
-
-  /* div > p:only-of-type {
-    text-align: start;
-    color: blue;
-  } */
-
-
-  div > p:nth-last-child(1) {
-    margin-bottom: 2em;
-  }
-
-  div > ul {    
-    display: grid;
-    justify-content: center;
-    align-items: center;
-    padding: 0em 2em;
-    grid-gap: 1em;
-    grid-template-columns: 1fr 1fr;
-  }
-
-  ul > li {
-    /* display: inline-block;
-    color: blue; */
-    /* display: flex;
-    flex-wrap: wrap; */
-  }
-
-`;
+import TypewriterAnimation from '../components/TypewriterAnimation';
+import AboutContainer from '../styles/sc-About';
 
 const About = (): JSX.Element => {
   console.log('bla');
   return (
     <SectionContainer>
+      <TypewriterAnimation />
       <AboutContainer>
+
         <div>
           <p>Olá, meu nome é Lucas.</p>
           <p>Sou um Desenvolvedor Full-Stack Jr.</p>
@@ -70,8 +31,8 @@ const About = (): JSX.Element => {
             <li>Express</li>
           </ul>
         </div>
-      </AboutContainer>
 
+      </AboutContainer>
     </SectionContainer>
   );
 };

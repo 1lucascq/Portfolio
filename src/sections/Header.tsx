4b25238c5929be406f5099d188b7c2e8dd044abe
lucas.quadros <1lucascq@gmail.com.br>
2024-09-05 08:42:05 -0300

    feat: Add Header component and update global styles

---
+++ src/sections/Header.tsx
@@ -0,0 +1,20 @@
+export const Header = () => {
+	return (
+		<header className='flex justify-center items-center fixed top-3'>
+			<nav className='flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blue'>
+				<a href='#' className='nav-item'>
+					Home
+				</a>
+				<a href='#' className='nav-item'>
+					Projects
+				</a>
+				<a href='#' className='nav-item'>
+					About
+				</a>
+				<a href='#' className='nav-item bg-white text-gray-900 hover:bg-white/80 hover:text-gray-900'>
+					Contact
+				</a>
+			</nav>
+		</header>
+	);
+};

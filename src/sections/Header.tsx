d256bd22949996458b91c03fdf20abd13a9a4e3b
lucas.quadros <1lucascq@gmail.com.br>
2024-09-05 13:33:24 -0300

    refactor: Update Header component to use function declaration syntax and add full width and z-index styles

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,6 @@
-export const Header = () => {
+export default function Header() {
 	return (
-		<header className='flex justify-center items-center fixed top-3'>
+		<header className='flex justify-center items-center fixed top-3 w-full z-30'>
 			<nav className='flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blue'>
 				<a href='#' className='nav-item'>
 					Home

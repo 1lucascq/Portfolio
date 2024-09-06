b1a92bfbd799d5a0016154fa6c81cb0ce7c26bab
lucas.quadros <1lucascq@gmail.com.br>
2024-09-06 01:59:53 -0300

    feat: Add Footer component for displaying the website footer

---
+++ src/sections/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <section>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, molestiae quis minima
+            saepe explicabo quod beatae fugit architecto neque repellat hic, libero ratione? Nihil
+            culpa amet iure sunt, illo quos.
+        </section>
+    );
+}

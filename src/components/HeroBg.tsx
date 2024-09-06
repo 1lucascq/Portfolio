aaaf3f960d68c2ce4270650ed80e6159b0a3375a
lucas.quadros <1lucascq@gmail.com.br>
2024-09-06 10:24:38 -0300

    refactor: Update HeroBg and HeroOrbit components

--- src/components/HeroBg.tsx
+++ src/components/HeroBg.tsx
@@ -5,6 +5,10 @@ import SparkleIcon from '@/assets/icons/sparkle.svg';
 import grainImage from '@/assets/images/grain.jpg';
 
 export default function HeroBg() {
+	const smallSpin = '5s';
+	const mediumSpin = '10s';
+	const bigSpin = '15s';
+
 	return (
 		<div className='absolute inset-0 [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_50%,transparent)]'>
 			<div className='absolute inset-0 -z-10 opacity-5' style={{ backgroundImage: `url(${grainImage.src})` }}></div>
@@ -12,35 +16,35 @@ export default function HeroBg() {
 			<div className='size-[820px] hero-ring' />
 			<div className='size-[1020px] hero-ring' />
 			<div className='size-[1220px] hero-ring' />
-			{/* <HeroOrbit size={800} rotation={-70}>
-				<StarIcon className='size-28 text-emerald-300' style={{ top: '10%', left: '10%' }} />
-			</HeroOrbit> */}
-			<HeroOrbit size={550} rotation={20}>
-				<StarIcon className='size-12 text-emerald-300' style={{ top: '10%', left: '10%' }} />
-			</HeroOrbit>
-			<HeroOrbit size={590} rotation={98}>
-				<StarIcon className='size-8 text-emerald-300' style={{ top: '10%', left: '10%' }} />
-			</HeroOrbit>
-			<HeroOrbit size={430} rotation={-14}>
+			<HeroOrbit size={430} rotation={-14} orbit orbitDuration="30s" spin spinDuration={smallSpin}>
 				<SparkleIcon className='size-8 text-emerald-300/20' style={{ top: '10%', left: '10%' }} />
 			</HeroOrbit>
-			<HeroOrbit size={440} rotation={79}>
+			<HeroOrbit size={440} rotation={79} orbit orbitDuration="33s" spin spinDuration={smallSpin}>
 				<SparkleIcon className='size-5 text-emerald-300/20' style={{ top: '10%', left: '10%' }} />
 			</HeroOrbit>
-			<HeroOrbit size={530} rotation={178}>
-				<SparkleIcon className='size-10 text-emerald-300/20' style={{ top: '10%', left: '10%' }} />
-			</HeroOrbit>
-			<HeroOrbit size={710} rotation={144}>
-				<SparkleIcon className='size-14 text-emerald-300/20' style={{ top: '10%', left: '10%' }} />
-			</HeroOrbit>
-			<HeroOrbit size={720} rotation={85}>
-				<div className='size-3 rounded-full bg-emerald-300/20' style={{ top: '10%', left: '10%' }} />
-			</HeroOrbit>
-			<HeroOrbit size={520} rotation={-41}>
+			<HeroOrbit size={520} rotation={-41} orbit orbitDuration="36s">
 				<div className='size-2 rounded-full bg-emerald-300/20' style={{ top: '10%', left: '10%' }} />
 			</HeroOrbit>
-			<HeroOrbit size={650} rotation={-5}>
+			<HeroOrbit size={530} rotation={178} orbit orbitDuration="39s" spin spinDuration={mediumSpin}>
+				<SparkleIcon className='size-10 text-emerald-300/20' style={{ top: '10%', left: '10%' }} />
+			</HeroOrbit>
+			<HeroOrbit size={550} rotation={20} orbit orbitDuration="42s" spin spinDuration={mediumSpin} >
+				<StarIcon className='size-12 text-emerald-300' style={{ top: '10%', left: '10%' }} />
+			</HeroOrbit>
+			<HeroOrbit size={590} rotation={98} orbit orbitDuration="45s" spin spinDuration={mediumSpin} >
+				<StarIcon className='size-8 text-emerald-300' style={{ top: '10%', left: '10%' }} />
+			</HeroOrbit>
+			<HeroOrbit size={650} rotation={-5} orbit orbitDuration="48s">
 				<div className='size-3 rounded-full bg-emerald-300/20' style={{ top: '10%', left: '10%' }} />
+			</HeroOrbit>
+			<HeroOrbit size={710} rotation={144} orbit orbitDuration="51s" spin spinDuration={bigSpin}>
+				<SparkleIcon className='size-14 text-emerald-300/20' style={{ top: '10%', left: '10%' }} />
+			</HeroOrbit>
+			<HeroOrbit size={720} rotation={85} orbit orbitDuration="54s">
+				<div className='size-3 rounded-full bg-emerald-300/20' style={{ top: '10%', left: '10%' }} />
+			</HeroOrbit>
+			<HeroOrbit size={800} rotation={-70} orbit orbitDuration="57s" spin spinDuration={bigSpin} >
+				<StarIcon className='size-28 text-emerald-300' style={{ top: '10%', left: '10%' }} />
 			</HeroOrbit>
 		</div>
 	);
--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,25 +1,55 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
-export default function HeroOrbit({ children, size, rotation }: PropsWithChildren<{ size: number; rotation: number }>) {
-	return (
-		<div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
-			<div
-                className='flex items-start justify-start'
-				style={{
-					transform: `rotate(${rotation}deg)`,
-					height: `${size}px`,
-					width: `${size}px`,
-				}}
-			>
-				<div
-					className='inline-flex'
-					style={{
-						transform: `rotate(${rotation * -1}deg)`,
-					}}
-				>
-					{children}
-				</div>
-			</div>
-		</div>
-	);
-};
+export default function HeroOrbit({
+    children,
+    size,
+    rotation,
+    orbitDuration,
+    orbit = false,
+    spin = false,
+    spinDuration,
+}: PropsWithChildren<{
+    size: number;
+    rotation: number;
+    orbitDuration?: string;
+    orbit?: boolean;
+    spinDuration?: string;
+    spin?: boolean;
+}>) {
+	// spin=false
+	// orbit=false
+    return (
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -z-20">
+            <div
+                className={twMerge(orbit && "animate-spin")}
+                style={{
+                    animationDuration: orbitDuration,
+                }}
+            >
+                <div
+                    className="flex items-start justify-start"
+                    style={{
+                        transform: `rotate(${rotation}deg)`,
+                        height: `${size}px`,
+                        width: `${size}px`,
+                    }}
+                >
+                    <div
+                        className={twMerge(spin && "animate-spin")}
+                        style={{ animationDuration: spinDuration }}
+                    >
+                        <div
+                            className="inline-flex"
+                            style={{
+                                transform: `rotate(${rotation * -1}deg)`,
+                            }}
+                        >
+                            {children}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}

a2dc1c5c612ed3d7ffdc77b0a7be9bc0d62787c0
lucas.quadros <1lucascq@gmail.com.br>
2024-09-06 09:53:05 -0300

    feat: Add animated ping effect to availability indicator

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,7 +25,18 @@ const config: Config = {
         sans: 'var(--font-sans)',
         serif: 'var(--font-serif)',
       },
-    },
+      animation: {
+        'ping-large': 'ping-large 1s ease-in-out infinite',
+      },
+      keyframes: {
+        'ping-large': {
+          '75%, 100%': {
+            transform: 'scale(3)',
+            opacity: '0',
+          }
+        }
+      }
+    }
   },
   plugins: [],
 };

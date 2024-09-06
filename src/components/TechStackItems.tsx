0200ba202a145a30dbfb5dc15ccc9c5f4f25ace5
lucas.quadros <1lucascq@gmail.com.br>
2024-09-05 23:16:22 -0300

    feat: Add TechStackItems component for displaying a list of tech stack items

---
+++ src/components/TechStackItems.tsx
@@ -0,0 +1,34 @@
+import { ElementType } from "react";
+import TechIcon from "@/components/TechIcon";
+import { twMerge } from "tailwind-merge";
+
+export default function TechStackItems({
+    items,
+    className,
+    itemsWrapperClassName,
+}: {
+    items: { title: string; iconType: ElementType }[];
+    className?: string;
+    itemsWrapperClassName?: string;
+}) {
+    return (
+        <div
+            className={twMerge(
+                "flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]",
+                className
+            )}
+        >
+            <div className={twMerge("flex flex-none py-0.5 gap-6 pr-6", itemsWrapperClassName)}>
+                {items.map((item) => (
+                    <div
+                        key={item.title}
+                        className="inline-flex items-center gap-4 py-2 px-3 outline outline-2 outline-white/10 rounded-lg"
+                    >
+                        <TechIcon component={item.iconType} />
+                        <span className="font-semibold">{item.title}</span>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}

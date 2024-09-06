a4dd2e2aba0896ee744af1ed55f6c6a3069ba166
lucas.quadros <1lucascq@gmail.com.br>
2024-09-06 01:22:34 -0300

    refactor: Improve Card and CardHeader components styling

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -3,12 +3,12 @@ import { twMerge } from "tailwind-merge";
 
 export default function CardHeader({ title, description, className }: { title: string; description: string; className?: string }) {
     return (
-        <div className={twMerge("flex flex-col", className)}>
+        <div className={twMerge("flex flex-col p-6 md:py-8 md:px-10", className)}>
             <div className="inline-flex items-center gap-2">
                 <StarIcon className="size-9 text-emerald-300" />
                 <h3 className="font-serif text-3xl">{title}</h3>
             </div>
-            <p className="text-sm text-white/60 mt-2">{description}</p>
+            <p className="text-sm text-white/60 mt-2 lg:text-base">{description}</p>
         </div>
     );
 }

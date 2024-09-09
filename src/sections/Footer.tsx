1a4f4aa874254f70559b6b163f7e0a7e361d9aeb
lucas.quadros <1lucascq@gmail.com.br>
2024-09-09 20:42:53 -0300

    refactor: Update Footer section component styles

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="relative -z-10 overflow-x-clip">
+        <footer className="relative -z-10 overflow-x-clip lg:pt-10">
             <div className="absolute h-[400px] w-[1600px] bottom-0 left=1/2 translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10" />
             <div className="container">
                 <div className="border-t border-white/15 py-6 text-sm flex flex-col items-center gap-8 md:flex-row md:justify-between">

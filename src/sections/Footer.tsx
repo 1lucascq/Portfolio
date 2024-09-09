f5284b8c52fbc868e1fc6c99c41a24b10f70203b
lucas.quadros <1lucascq@gmail.com.br>
2024-09-09 19:10:53 -0300

    refactor: Remove unused code and update footer layout

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,44 +1,10 @@
-import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
-
 export default function Footer() {
-    const links = [
-        {
-            title: "LinkedIn",
-            href: "#",
-        },
-        {
-            title: "GitHub",
-            href: "#",
-        },
-        {
-            title: "Projects",
-            href: "#",
-        },
-        {
-            title: "Contact",
-            href: "#",
-        },
-    ];
-
     return (
         <footer className="relative -z-10 overflow-x-clip">
             <div className="absolute h-[400px] w-[1600px] bottom-0 left=1/2 translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10" />
-                <div className="container">
-                    <div className="border-t border-white/15 py-6 text-sm flex flex-col items-center gap-8 md:flex-row md:justify-between">
-                        <div className="text-white/40">&copy; 2024. All rights reserved.</div>
-                        <nav className="flex flex-col items-center gap-8 md:flex-row">
-                            {links.map((link) => (
-                                <a
-                                    key={link.title}
-                                    href={link.href}
-                                    className="inline-flex items-center gap-1.5"
-                                >
-                                    <span className="font-semibold">{link.title}</span>
-                                    <ArrowUpRightIcon className="size-4" />
-                                </a>
-                            ))}
-                        </nav>
-                    {/* </div> */}
+            <div className="container">
+                <div className="border-t border-white/15 py-6 text-sm flex flex-col items-center gap-8 md:flex-row md:justify-between">
+                    <div className="text-white/40">&copy; 2024. All rights reserved.</div>
                 </div>
             </div>
         </footer>

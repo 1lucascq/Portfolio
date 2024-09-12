3dab04e4a37a480cf6f70de2da9ff72705041f1d
lucas.quadros <1lucascq@gmail.com.br>
2024-09-12 00:22:48 -0300

    refactor: Update Contact section component styles and Footer section component styles

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -3,7 +3,7 @@ import grainImage from "@/assets/images/grain.jpg";
 
 export default function ContactSection() {
     return (
-        <section id="contact" className="py-16 pt-12 lg:py-24 lg:pt-22">
+        <section id="contact" className="py-24 lg:py-24 lg:pt-22">
             <div className="container">
                 <div
                     className="bg-gradient-to-b from-primary to-secondary text-gray-900 py-8 px-10 rounded-3xl text-center relative overflow-hidden z-0 md:text-left lg:py-16"
